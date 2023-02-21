322b1bb09cd8d59bd70169b083b057da0c7870cb
VolodymyrKurdiukov <volodymyr.kurdiukov@gmail.com>
2023-02-21 20:00:43 +0100

    not done

---
+++ lesson_3/task1/js/script.js
@@ -0,0 +1,4 @@
+let boyName = prompt('Введіть імя хлопчика', '');
+let girlName = prompt('Введіть імя дівчинки', '');
+let boyCandies = parseInt(prompt('Введіть rskmrscn', ''));
+let girlName = prompt('Введіть імя дівчинки', '');
